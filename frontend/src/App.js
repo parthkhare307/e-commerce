048756b7629a45391222282ad229e8c93d60db9c
Parth Khare <parthkhare307@gmail.com>
2024-01-07 01:49:06 +0530

    Working on Product Display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
 
         <Route path="/product" element={<Product/>}>
-          <Route path='productId' element={<Product/>} />
+          <Route path=':productId' element={<Product/>} />
         </Route>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
