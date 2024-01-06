4307c89f384099c48bc72408b35d3f2401ef9da0
Parth Khare <parthkhare307@gmail.com>
2024-01-06 23:25:19 +0530

    Working On ShopCategory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png';
+import women_banner from './Components/Assets/banner_women.png';
+import kids_banner from './Components/Assets/banner_kids.png';
 
 
 function App() {
@@ -17,9 +20,9 @@ function App() {
       <Routes>
 
       <Route path='/' element={<Shop/>}/>
-      <Route path='/mens' element={<ShopCategory category="men"/>}/>
-      <Route path='/womens' element={<ShopCategory category="women"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+      <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+      <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+      <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
 
         <Route path="/product" element={<Product/>}>
           <Route path='productId' element={<Product/>} />
