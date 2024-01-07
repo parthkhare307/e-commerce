5bf120f4d26fddf68190ef036d19779e09cfcb4a
Parth Khare <parthkhare307@gmail.com>
2024-01-07 07:41:53 +0530

    Completed The Product display

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -20,7 +20,7 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right">
             <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
+            <div className="productdisplay-right-stars">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -28,6 +28,27 @@ const ProductDisplay = (props) => {
                 <img src={start_dull_icon} alt="" />
                 <p>(122)</p>
             </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                A lightweight, usually knitted, pullover shirt, close-fitting and a round neckline and short
+                sleeves, worn as an undershirt or outer garment. 
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-size">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tags :</span>Modern, Latest</p>
         </div>
     </div>
   )
