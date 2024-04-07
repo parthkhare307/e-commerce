4a875c497aa01f753d4d81ec96e698a14ab06c49
Parth Khare <parthkhare307@gmail.com>
2024-04-08 04:02:42 +0530

    Added The Dropdown menu in Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState, useContext, useRef } from 'react'
 import './Navbar.css'
 import {Link} from 'react-router-dom'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import {ShopContext} from '../../Context/ShopContext'
+import nav_dropdown from '../Assets/nav_dropdown_icon.webp'
 
 const Navbar = () => {
 
   const [menu,setMenu] = useState("shop");
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
@@ -15,7 +23,8 @@ const Navbar = () => {
         <img src={logo} alt="" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={()=> {setMenu("shop")}}><Link style={{textDecoration: 'none' }} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=> {setMenu("mens")}}><Link style={{textDecoration: 'none' }} to='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
         <li onClick={()=> {setMenu("womens")}}><Link style={{textDecoration: 'none' }} to='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
