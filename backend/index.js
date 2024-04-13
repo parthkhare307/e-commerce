1ae23690842925d75e1dff1b8c91b75c2c35030d
Parth Khare <parthkhare307@gmail.com>
2024-04-13 20:51:54 +0530

    Working on Backend

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,28 @@ app.get("/",(req,res)=>{
     res.send("Express App is Running")
 })
 
+// Image Storage Engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+// Creating Upload Endpoint for images
+
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload", upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
 app.listen(port,(error)=>{
     if(!error){
         console.log("Server Running on Port "+port)
