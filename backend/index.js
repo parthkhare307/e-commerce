3eb850c9ddce36237f22dce8225de61a02758976
Parth Khare <parthkhare307@gmail.com>
2024-04-15 23:16:22 +0530

    Created The API For Product Upload and Remove

--- backend/index.js
+++ backend/index.js
@@ -80,8 +80,19 @@ const Product = mongoose.model("Product",{
 })
 
 app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
     const product = new Product({
-        id:req.body.id,
+        id: id,
         name:req.body.name,
         image:req.body.image,
         category:req.body.category,
@@ -94,6 +105,17 @@ app.post('/addproduct',async (req,res)=>{
     res.json({
         success:true,
         name:req.body.name,
+    })
+})
+
+// Creating API for deleting Proucts
+
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
     })
 })
 
