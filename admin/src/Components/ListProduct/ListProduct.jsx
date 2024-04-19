7007550d744a914f2f7f0e6253aa565a3deb5d76
Parth Khare <parthkhare307@gmail.com>
2024-04-20 05:06:53 +0530

    Styling of Product list component is Completed

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -18,8 +18,20 @@ const ListProduct = () => {
     fetchInfo();
   },[])
 
+  const remove_product = async (id)=>{
+    await fetch('http://localhost:4000/removeproduct',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify({id:id})
+    })
+    await fetchInfo();
+  }
+
   return (
-    <div className='listProduct'>
+    <div className='list-product'>
       <h1>All Product List</h1>
       <div className="listproduct-format-main">
         <p>Products</p>
@@ -33,14 +45,16 @@ const ListProduct = () => {
         <hr />
 
         {allproducts.map((product,index)=>{
-          return <div key={index} className="listproduct-format-main listproduct-format">
+          return <> <div key={index} className="listproduct-format-main listproduct-format">
               <img src={product.image} alt="" className="listproduct-product-icon" />
               <p>{product.name}</p>
               <p>${product.old_price}</p>
               <p>${product.new_price}</p>
               <p>{product.category}</p>
-              <img src={cross_icon} alt="" className="listproduct-remove-icon" />
+              <img onClick={()=>{remove_product(product.id)}} src={cross_icon} alt="" className="listproduct-remove-icon" />
           </div>
+          <hr />  
+          </>
         })}
 
 
